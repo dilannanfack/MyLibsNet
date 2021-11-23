aaad08a2d9ba76f395649dc2aabefafd1945e4d0
dilan costa <dilannanfack@gmail.com>
2021-11-23 23:44:45 +0100

    Ajoutez des fichiers projet.

---
+++ MyLibsNet.Serialisation/Serializer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace MyLibs.Serialisation
+{
+    public class Serializer
+    {
+
+        public void Serialize<T>(T data, String path)
+        {
+            using (StreamWriter file = new StreamWriter(path, true))
+            {
+                BinaryFormatter bf = BinaryFormatter();
+                bf.Serialize(file.BaseStream, data);
+            }
+        }
+
+        private BinaryFormatter BinaryFormatter()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Deserialize<T>(string path)
+        {
+            using (StreamReader file = new StreamReader(path, true))
+            {
+                BinaryFormatter bf = BinaryFormatter();
+                return (T)bf.Deserialize(file.BaseStream);
+            }
+        }
+    }
+}
